28986ef57787679b3607a05abd213409292c9567
Hani Boubker <h.boubker@live.be>
2024-01-27 23:37:42 +0100

    refractor(open_note): - le cursor passe en pointer quand on passe sur les notes - les notes sont remplacé par des balise <a> au lieu de faire un onclick.

--- view/tools.php
+++ view/tools.php
@@ -10,66 +10,68 @@ function show_note(array $arr_notes, string $title, string $titlePage): void
             $noteType = determineNoteType($titlePage);
             $openNoteUrl = "./Notes/open_note/" . $note->id . "/" . $noteType;
         ?>
-            <li class="note" onclick="window.location='<?= $openNoteUrl ?>'">
-                <div class="header-in-note"><?= $note->title ?></div>
-                <div class="body-note">
-                    <?php if (property_exists($note, 'content')) {
-                        $max_lg = 75;
-                        $content = $note->content === null ? "" : $note->content;
-                        $content_sub = strlen($content) > $max_lg ? substr($content, 0, $max_lg) . "..."  : $content;
-                    ?>
-                        <p class='card-text mb-0'><?= $content_sub ?></p>
-                        <?php
+            <li class="note">
+                <a href="<?= $openNoteUrl ?>">
 
-                    } else {
-                        $items = $note->list_item;
-                        $list_item_showable = count($items) > 3 ? array_slice($items, 0, 3) : $items;
-                        foreach ($list_item_showable as $item) :
-                            $max_lg = 15;
-                            $content = $item->content;
-                            $content_sub = strlen($content) > $max_lg ? substr($content, 0, $max_lg) . "..." : $content;
+                    <div class="header-in-note"><?= $note->title ?></div>
+                    <div class="body-note">
+                        <?php if (property_exists($note, 'content')) {
+                            $max_lg = 75;
+                            $content = $note->content === null ? "" : $note->content;
+                            $content_sub = strlen($content) > $max_lg ? substr($content, 0, $max_lg) . "..."  : $content;
                         ?>
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" <?= $item->checked ? "checked" : ""  ?> disabled>
-                                <label class="form-check-label" for="flexCheckDefault">
-                                    <?= $content_sub ?>
-                                </label>
-                            </div>
+                            <p class='card-text mb-0'><?= $content_sub ?></p>
+                            <?php
+                        } else {
+                            $items = $note->list_item;
+                            $list_item_showable = count($items) > 3 ? array_slice($items, 0, 3) : $items;
+                            foreach ($list_item_showable as $item) :
+                                $max_lg = 15;
+                                $content = $item->content;
+                                $content_sub = strlen($content) > $max_lg ? substr($content, 0, $max_lg) . "..." : $content;
+                            ?>
+                                <div class="form-check">
+                                    <input class="form-check-input cursor-pointer" type="checkbox" value="" id="flexCheckDefault" <?= $item->checked ? "checked" : ""  ?> disabled>
+                                    <label class="form-check-label cursor-pointer" for="flexCheckDefault">
+                                        <?= $content_sub ?>
+                                    </label>
+                                </div>
+                            <?php
+                            endforeach;
+                            if (count($items) > 3) {
+                            ?>
+                                <p class="card-text">...</p>
                         <?php
-                        endforeach;
-                        if (count($items) > 3) {
-                        ?>
-                            <p class="card-text">...</p>
-                    <?php
+                            }
                         }
+                        ?>
+                    </div>
+                    <?php
+                    if ($titlePage === Page::Notes->value) {
+                    ?>
+                        <form class="footer-in-note" action="notes/" method="post">
+                            <?php if ($i !== 0) {
+                            ?>
+                                <button class="button-mv-note" type="submit" name="action" value="increment">
+                                    <i class="bi bi-chevron-double-left icon-mv-note i-left"></i>
+                                </button>
+                            <?php
+                            }
+                            if ($note->id != end($arr_notes)->id) {
+                            ?>
+                                <button class="button-mv-note" type="submit" name="action" value="decrement">
+                                    <i class="bi bi-chevron-double-right icon-mv-note i-right"></i>
+                                </button>
+                            <?php
+                            }
+                            ?>
+                            <input type="hidden" name="id" value=<?= $note->id ?>>
+
+                        </form>
+                    <?php
                     }
                     ?>
-                </div>
-                <?php
-                if ($titlePage === Page::Notes->value) {
-                ?>
-                    <form class="footer-in-note" action="notes/" method="post">
-                        <?php if ($i !== 0) {
-                        ?>
-                            <button class="button-mv-note" type="submit" name="action" value="increment">
-                                <i class="bi bi-chevron-double-left icon-mv-note i-left"></i>
-                            </button>
-                        <?php
-                        }
-                        if ($note->id != end($arr_notes)->id) {
-                        ?>
-                            <button class="button-mv-note" type="submit" name="action" value="decrement">
-                                <i class="bi bi-chevron-double-right icon-mv-note i-right"></i>
-                            </button>
-                        <?php
-                        }
-                        ?>
-                        <input type="hidden" name="id" value=<?= $note->id ?>>
-
-                    </form>
-                <?php
-                }
-                ?>
+                </a>
 
             </li>
         <?php };
