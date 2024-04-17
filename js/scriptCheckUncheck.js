292ff472f6fb02f5fb34ea8fd724333212f34133
Rayan Zian <rayan-zian@hotmail.fr>
2024-04-17 19:48:13 +0200

    suppression des console.log

--- js/scriptCheckUncheck.js
+++ js/scriptCheckUncheck.js
@@ -5,29 +5,17 @@ $(() => {
         let itemId = $(this).closest('.input-group').find('input[name="item_id"]').val();
         let noteId = $(this).closest('.input-group').find('input[name="note_id"]').val();
 
-        console.log(itemId);
-        console.log(noteId);
-
-
         $.ajax({
             url: 'notes/toggle_checkbox_service',
             method: 'POST',
             data: { item_id: itemId, note_id: noteId }
         }).done(function(response) {
 
-            console.log(response);
-
             var jsonResponse = JSON.parse(response);
-
             var checked = jsonResponse['\u0000ChecklistNoteItems\u0000checked'];
-
-            console.log("Checked:",checked);
-
             var checkbox = $("#checkbox_" + itemId);
 
             checkbox.prop('checked', checked);
-
         });
-
     });
 });
