8dc7dcc091e5bc70683a24b02ae18461ff45f23c
Ayman <0312ayhandali@student.epfc.eu>
2024-04-19 20:12:49 +0200

    feat(validation): changement du bouton en gris

--- js/scriptValidateTextNote.js
+++ js/scriptValidateTextNote.js
@@ -25,47 +25,12 @@ $(document).ready(function() {
         validateText();
     });
 
-    function validateTitle1() {
-        var title = $('#title_add_text_note').val().trim();
-        if (title.length === 0) {
-            $('#title_add_text_note').attr('required', true);
-            $('#title_error').text('');
-        } else if (title.length < minTitleLength || title.length > maxTitleLength) {
-            $('#title_add_text_note').addClass('is-invalid');
-            $('#title_error').text('Le titre doit avoir entre ' + minTitleLength + ' et ' + maxTitleLength + ' caractères.');
-        } else {
-            $.ajax({
-                url: 'notes/checkUniqueTitle',
-                type: 'POST',
-                data: {
-                    title: title,
-                    noteId: noteId
-                },
-                dataType: 'json',
-                success: function(response) {
-                    console.log(response)
-                    if (!response.unique) {
-                        $('#title_add_text_note').addClass('is-invalid');
-                        $('#title_error').text('Ce titre est déjà utilisé.');
-                    } else {
-                        $('#title_add_text_note').removeClass('is-invalid').addClass('is-valid');
-                        $('#title_error').text('');
-                    }
-                    enableSaveButtonIfValid();
-                },
-                error: function(xhr, status, error) {
-                    console.error(error);
-                }
-            });
-        }
-        enableSaveButtonIfValid();
-    }
     function validateTitle() {
         var title = $('#title_add_text_note').val().trim();
         var noteId = $('#noteId').length ? $('#noteId').data('note-id') : -1;
         console.log(noteId)
         if (title.length === 0) {
-            $('#title_add_text_note').attr('required', true);
+            $('#title_add_text_note');
             $('#title_error').text('');
         } else if (title.length < minTitleLength || title.length > maxTitleLength) {
             $('#title_add_text_note').addClass('is-invalid');
@@ -116,11 +81,12 @@ $(document).ready(function() {
     function enableSaveButtonIfValid() {
         var titleIsValid = !$('#title_add_text_note').hasClass('is-invalid');
         var textIsValid = !$('#text_add_text_note').hasClass('is-invalid');
+        var titleLength = $('#title_add_text_note').val().trim().length;
 
-        if (titleIsValid && textIsValid) {
-            $('#save_button').prop('disabled', false).removeClass('disabled').show();
+        if (titleIsValid && textIsValid && titleLength > 0) {
+            $('#save_button').prop('disabled', false).removeClass('disabled-button');
         } else {
-            $('#save_button').prop('disabled', true).addClass('disabled').hide();
+            $('#save_button').prop('disabled', true).addClass('disabled-button');
         }
     }
 });
