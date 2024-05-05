f5f261d5585d829b1cffe3ba4356c9f2162ff8d8
Hani Boubker <h.boubker@live.be>
2024-05-05 21:37:39 +0200

    fix(modal_confirm): la modal s'affiche quand un item à été modifié et qu'on essaye de quitter l'edition

--- js/scriptEditChecklistNote.js
+++ js/scriptEditChecklistNote.js
@@ -18,15 +18,13 @@ $(() => {
 
   // fonction pour gérer la frappe clavier dans les champs edit_item
   function handleItemKeyPress() {
+    //TO DO il faut que les items se savent uniquement au moment ou on clique sur le bouton save pas à chaque fois qu'on clique sur une touche
     $('[id^="item"]').keyup(function (event) {
       let itemId = $(this)
         .closest(".input-group")
         .find('input[name="item_id"]')
         .val();
-      let noteId = $(this)
-        .closest(".input-group")
-        .find('input[name="note_id"]')
-        .val();
+
       let itemElem = $(this);
       let item = itemElem.val();
 
@@ -43,12 +41,8 @@ $(() => {
   }
 
   // fonction pour gérer la frappe clavier dans le champ new_item
-  function handleAddKeyPress(){
-    $("#add_item").keyup(function() {
-      let noteId = $(this)
-          .closest(".input-group")
-          .find('input[name="note_id"]')
-          .val();
+  function handleAddKeyPress() {
+    $("#add_item").keyup(function () {
       let content = $(this).val();
       $.ajax({
         url: "notes/check_new_item_service",
@@ -63,9 +57,9 @@ $(() => {
             $("#add_item").removeClass("is-valid");
             $("#add_button").prop("disabled", true);
           } else {
-            let html = "<span class=\"error-add-note\" id=\"new_item_error\">"
+            let html = '<span class="error-add-note" id="new_item_error">';
             html += jsonResponse.new_item;
-            html += "</span>"
+            html += "</span>";
             $("#new_item_error_div").html(html);
             $("#add_item").removeClass("is-valid");
             $("#add_item").addClass("is-invalid");
@@ -82,20 +76,19 @@ $(() => {
   }
 
   // fonction pour gérer la frappe clavier dans le champ titre
-  function handleTitleKeyPress(){
+  function handleTitleKeyPress() {
     $("#titleNote").keyup(function (event) {
-      let notedId = $('input[name="note_id"]').val();
       let newContent = $("#titleNote").val();
 
       $.ajax({
         url: "notes/edit_title_service",
         method: "POST",
-        data: { note_id: noteId, title: newContent},
+        data: { note_id: noteId, title: newContent },
       }).done(function (response) {
         let jsonResponse = JSON.parse(response);
         if ("title" in jsonResponse.errors) {
           $("#save_button").prop("disabled", true).css("opacity", "0.3");
-          let html = "<span class=\"error-add-note\" id=\"error_title_span\">";
+          let html = '<span class="error-add-note" id="error_title_span">';
           html += jsonResponse.errors.title;
           html += "</span>";
           $("#error_title_span").remove();
@@ -108,18 +101,12 @@ $(() => {
     });
   }
 
-
   // fonction pour gérer le clic sur les boutons remove_item
   function handleRemoveClick() {
     $("button[name='remove_button']").each(function () {
       $(this).click(function (e) {
         e.preventDefault();
         let itemId = $(this).val();
-
-        let noteId = $(this)
-            .closest(".input-group")
-            .find('input[name="note_id"]')
-            .val();
 
         $.ajax({
           url: "notes/remove_item_service",
@@ -133,16 +120,10 @@ $(() => {
     });
   }
 
-
   // fonction pour gérer le clic sur le bouton add_item
   function handleAddClick() {
-    $("#add_button").click(function (e)  {
+    $("#add_button").click(function (e) {
       e.preventDefault();
-
-      let noteId = $(this)
-          .closest(".input-group")
-          .find('input[name="note_id"]')
-          .val();
 
       let newItem = $("#add_item").val();
 
@@ -153,16 +134,14 @@ $(() => {
       }).done(function (response) {
         let jsonResponse = JSON.parse(response);
 
-
         let itemList = displayItems(jsonResponse);
 
-        $('#list_items_ul').html(itemList);
+        $("#list_items_ul").html(itemList);
 
-        $("#add_item").val('');
+        $("#add_item").val("");
         $("#add_item").removeClass("is-valid");
         $("#save_button").prop("disabled", false).css("opacity", "1");
       });
-
     });
   }
 
@@ -172,13 +151,9 @@ $(() => {
       event.preventDefault();
 
       let itemId = $(this)
-          .closest(".input-group")
-          .find('input[name="item_id"]')
-          .val();
-      let noteId = $(this)
-          .closest(".input-group")
-          .find('input[name="note_id"]')
-          .val();
+        .closest(".input-group")
+        .find('input[name="item_id"]')
+        .val();
 
       $.ajax({
         url: "notes/toggle_checkbox_service",
@@ -198,7 +173,6 @@ $(() => {
 });
 
 function displayItem(itemJson, itemElem) {
-
   // si c'est une array il n'y a pas d'erreur, si c'est un json il y a des erreurs
   if (Array.isArray(itemJson.errors)) {
     $("#save_button").prop("disabled", false).css("opacity", "1");
@@ -212,7 +186,7 @@ function displayItem(itemJson, itemElem) {
       itemElem.removeClass("is-valid");
       itemElem.addClass("is-invalid");
       let html = "";
-/*
+      /*
       for (let itemId in itemJson.errors) {
         for (let error of itemJson.errors[itemId]) {
           html +=
@@ -228,7 +202,6 @@ function displayItem(itemJson, itemElem) {
         }
       }
       */
-
 
       for (let error of itemJson.errors["item" + itemJson.id]) {
         html +=
@@ -247,27 +220,41 @@ function displayItem(itemJson, itemElem) {
 }
 
 function displayItems(itemsJson) {
-  let html = "<ul class=\"list-unstyled\">";
+  let html = '<ul class="list-unstyled">';
   for (let i of itemsJson) {
-
-    html += "<li class=\"list-unstyled\" id=\"list_items_" + i.id + "\">";
-    html += "<div class=\"input-group pt-3 has-validation\">";
-    html += "<div class=\"input-group-text bg-primary  border-secondary \">";
-    html += "<input class=\"form-check-input border align-middle \" type=\"checkbox\" name=\"checked\" value=\"1\"" + (i.checked ? 'checked' : '') + " aria-label=\"Checkbox for following text input\" disabled>";
+    html += '<li class="list-unstyled" id="list_items_' + i.id + '">';
+    html += '<div class="input-group pt-3 has-validation">';
+    html += '<div class="input-group-text bg-primary  border-secondary ">';
+    html +=
+      '<input class="form-check-input border align-middle " type="checkbox" name="checked" value="1"' +
+      (i.checked ? "checked" : "") +
+      ' aria-label="Checkbox for following text input" disabled>';
     html += "</div>";
-    html += "<input value=\"" + i.content + "\" type=\"text\" name=\"item" + i.id + "\" class=\"form-control bg-secondary text-white bg-opacity-25 border-secondary\" id=\"item" + i.id + "\" >";
+    html +=
+      '<input defaultValue="' +
+      i.content +
+      '"value="' +
+      i.content +
+      '" type="text" name="item' +
+      i.id +
+      '" class="item-editable form-control bg-secondary text-white bg-opacity-25 border-secondary" id="item' +
+      i.id +
+      '" >';
     // attribut supprimé à l'input au-dessus
     // value="<?php echo isset($_POST['item' . $item->get_Id()]) ? htmlspecialchars($_POST['item' . $item->get_Id()]) : ''; ?>"
-    html += "<button name=\"remove_button\" value=\"" + i.id + "\" class=\"btn btn-danger btn-lg rounded-end  border-secondary\" type=\"submit\">";
-    html += "<i class=\"bi bi-x\"></i>";
+    html +=
+      '<button name="remove_button" value="' +
+      i.id +
+      '" class="btn btn-danger btn-lg rounded-end  border-secondary" type="submit">';
+    html += '<i class="bi bi-x"></i>';
     html += "</button>";
-    html += "<input type=\"hidden\" name=\"item_id\" value=\"" + i.id + "\">";
-    html += "<input type=\"hidden\" name=\"note_id\" value=\"" + i.checklist_note + "\">";
+    html += '<input type="hidden" name="item_id" value="' + i.id + '">';
+    html +=
+      '<input type="hidden" name="note_id" value="' + i.checklist_note + '">';
     html += "</div>";
     html += "</li>";
   }
   return html;
 }
-
 
 const refresh = () => {};
