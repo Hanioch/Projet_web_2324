31eafb7c8e1cc7f55a891a3e00e24d6b09d9087e
Rayan Zian <rayan-zian@hotmail.fr>
2024-05-07 00:13:28 +0200

    fix(edit_labels): correction du auto-completion

--- view/view_edit_labels.php
+++ view/view_edit_labels.php
@@ -39,7 +39,7 @@ include("./utils/header_edit_labels.php");
                         } else {
                             echo 'value=""';
                         } ?>
-                list="label_data_list" type="text" name="new_label" class="form-control bg-secondary text-white bg-opacity-25 border-secondary" id="add_label" placeholder="Type to search or create...">
+                list="label_data_list" type="text" name="new_label" class="form-control bg-secondary text-white bg-opacity-25 border-secondary" id="add_label" placeholder="Type to search or create..." autocomplete="off">
                 <button id="add_button" name="add_button" class="btn btn-primary btn-lg rounded-end  border-secondary" type="submit">
                     <i class="bi bi-plus"></i>
                 </button>
