3da89889cc105bab14ce381266f505359b3001eb
Hani Boubker <h.boubker@live.be>
2024-05-11 20:29:11 +0200

    fix(): quand on appuyait sur enter apres avoir rentrer un label, ça supprimait le dernier au lieu d'ajouter le label

--- view/view_edit_labels.php
+++ view/view_edit_labels.php
@@ -8,9 +8,9 @@ include("./utils/header_edit_labels.php");
 <div class="card-body">
     <span id="idNote" value="<?= $note->get_Id() ?>" style="display: none;"></span>
 
-    <form id="<?= $id_form ?>" class="p-3 text-white " action="notes/edit_labels/<?= $note->get_Id() ?>" method="post">
+    <div class="p-3 text-white ">
 
-        <div class="mb-3">
+        <form id="<?= $id_form ?>" action="notes/edit_labels/<?= $note->get_Id() ?>" method="post" class="mb-3">
             <div class="">
                 <label class="form-label mb-0 fs-5">Labels : </label>
                 <ul class="list-unstyled" id="list_labels">
@@ -23,7 +23,7 @@ include("./utils/header_edit_labels.php");
                         <li class="list-unstyled" id="list_labels_<?= $label->get_label_name() ?>">
                             <div class="input-group pt-3 has-validation">
                                 <input readonly value="<?= $label->get_label_name() ?>" type="text" name="label<?php echo $label->get_id() ?>" class="form-control bg-secondary text-white bg-opacity-25 border-secondary" id="label<?php echo $label->get_id() ?>">
-                                <button name="remove_button" value="<?= $label->get_label_name() ?>" class="btn btn-danger btn-lg rounded-end  border-secondary" type="submit">
+                                <button form="<?= $id_form ?>" name="remove_button" value="<?= $label->get_label_name() ?>" class="btn btn-danger btn-lg rounded-end  border-secondary" type="submit">
                                     <i class="bi bi-x"></i>
                                 </button>
                             </div>
@@ -31,9 +31,9 @@ include("./utils/header_edit_labels.php");
                     <?php endforeach; ?>
                 </ul>
             </div>
-        </div>
+        </form>
         <label for="add_label" class="form-label">Add a new label : </label>
-        <div class="input-group">
+        <form action="notes/edit_labels/<?= $note->get_Id() ?>" method="post" class="input-group">
             <input <?php if (!empty($errors['label'])) {
                         echo 'value="' . $_POST['new_label'] . '" class="form-control bg-secondary text-white bg-opacity-25 border-secondary is-invalid"';
                     } else {
@@ -49,18 +49,18 @@ include("./utils/header_edit_labels.php");
                 <?php }
                 ?>
             </datalist>
-        </div>
+        </form>
         <div id="error_div">
             <?php if (isset($errors['label'])) : ?>
                 <div id="new_label_error_div" class="error-add-note pt-1">
                     <?php foreach ($errors['label'] as $error) {
                     ?> <div> <?php
-                                        echo $error;
-                                        ?> </div> <?php
-                                    } ?></div>
+                                echo $error;
+                                ?> </div> <?php
+                                        } ?></div>
             <?php endif; ?>
         </div>
-    </form>
+    </div>
 </div>
 
 <script>
