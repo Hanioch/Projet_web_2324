2bcbf9a9a41633e5ee4015609d0b6df71a8d3e94
Ayman <0312ayhandali@student.epfc.eu>
2024-05-17 11:51:01 +0200

    feat(develop): var en let

--- js/scriptSearch.js
+++ js/scriptSearch.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $('input[type="checkbox"]').change(async function() {
-        var filters = {};
+        let filters = {};
         $('input[type="checkbox"]:checked').each(function() {
-            var name = $(this).attr('name');
+            let name = $(this).attr('name');
             if (name) {
                 filters[name] = 'on';
             }
@@ -42,22 +42,22 @@ $(document).ready(function() {
 
 function updateURL(response) {
     if (response.list_filter_encoded != null && response.list_filter_encoded.trim() !== '') {
-        var baseUrl = 'http://localhost/prwb_2324_a04/notes/search';
-        var newUrl = baseUrl + '/' + response.list_filter_encoded;
+        let baseUrl = 'http://localhost/prwb_2324_a04/notes/search';
+        let newUrl = baseUrl + '/' + response.list_filter_encoded;
         history.pushState(null, null, newUrl);
     } else {
-        var currentUrl = window.location.href;
-        var urlParts = currentUrl.split('/');
+        let currentUrl = window.location.href;
+        let urlParts = currentUrl.split('/');
         if (urlParts[urlParts.length - 1].match(/^[0-9a-zA-Z]+$/)) {
             urlParts.pop();
         }
-        var newUrl = urlParts.join('/');
+        let newUrl = urlParts.join('/');
         history.pushState(null, null, newUrl);
     }
 }
 
 function isEmptyObject(obj) {
-    for (var key in obj) {
+    for (let key in obj) {
         if (Object.prototype.hasOwnProperty.call(obj, key)) {
             return false;
         }
@@ -81,11 +81,11 @@ function sortSharedNotes(shared_notes) {
 
 function show_notes(arrNotes, title, titlePage, param, sectionClass, append = false) {
     return new Promise((resolve, reject) => {
-        var html = '';
+        let html = '';
         html += '<h4 class="title-note">' + title + '</h4>';
-        var isParamExist = !!param;
+        let isParamExist = !!param;
         html += '<ul id="sortable" class="list-note connectedSortable">';
-        var labelPromises = arrNotes.map(function(note) {
+        let labelPromises = arrNotes.map(function(note) {
             return new Promise(function(resolve, reject) {
                 $.ajax({
                     type: 'POST',
@@ -106,7 +106,7 @@ function show_notes(arrNotes, title, titlePage, param, sectionClass, append = fa
         Promise.all(labelPromises)
             .then(function(labelResponses) {
                 arrNotes.forEach(function(note, index) {
-                    var openNoteUrl = "./Notes/open_note/" + note.id;
+                    let openNoteUrl = "./Notes/open_note/" + note.id;
                     if (isParamExist) {
                         openNoteUrl += "/" + param;
                     }
@@ -116,16 +116,16 @@ function show_notes(arrNotes, title, titlePage, param, sectionClass, append = fa
                     html += '<div class="header-in-note">' + note.title + '</div>';
                     html += '<div class="body-note">';
                     if (note.content != null && note.content !== '') {
-                        var maxLg = 75;
-                        var contentSub = note.content.length > maxLg ? note.content.substring(0, maxLg) + "..." : note.content;
+                        let maxLg = 75;
+                        let contentSub = note.content.length > maxLg ? note.content.substring(0, maxLg) + "..." : note.content;
                         html += '<p class="card-text mb-0">' + contentSub + '</p>';
                     } else {
-                        var items = note.list_item;
+                        let items = note.list_item;
                         if (items && items.length > 0) {
-                            var listItemShowable = items.length > 3 ? items.slice(0, 3) : items;
+                            let listItemShowable = items.length > 3 ? items.slice(0, 3) : items;
                             listItemShowable.forEach(function(item) {
-                                var maxLg = 15;
-                                var contentSub = item.content.length > maxLg ? item.content.substring(0, maxLg) + "..." : item.content;
+                                let maxLg = 15;
+                                let contentSub = item.content.length > maxLg ? item.content.substring(0, maxLg) + "..." : item.content;
                                 html += '<div class="form-check">';
                                 html += '<input class="form-check-input cursor-pointer" type="checkbox" value=""' + (item.checked ? ' checked' : '') + ' disabled>';
                                 html += '<label class="form-check-label cursor-pointer">' + contentSub + '</label>';
@@ -140,7 +140,7 @@ function show_notes(arrNotes, title, titlePage, param, sectionClass, append = fa
                     }
                     html += '</div>';
                     html += '<div class="form-check">';
-                    var labels = labelResponses[index];
+                    let labels = labelResponses[index];
                     if (labels && labels.length > 0) {
                         html += '<form action="notes/edit_labels/' + note.id + '" method="POST" class="navbar-brand d-inline-block">';
                         html += '<input type="hidden" name="note_id" value="' + note.id + '">';
